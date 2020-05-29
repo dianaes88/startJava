5fb22900de8f0a4b6340b4052a6dee1a5cb7a659
Diana Eskina <diana.s.eskina@yandex.ru>
2020-05-29 11:57:39 +0300

    Person class is added

---
+++ 2dLesson/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+
+    private String name;
+    private String male;
+    private int age;
+    private float weight;
+    public float height;
+
+    private Person(String name, String male, int age, float weight, float height) {
+        this.name = name;
+        this.male = male;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Person() {
+        this.name = "Ivan";
+        this.male = "man";
+        this.age = 20;
+        this.weight = 80.0f;
+        this.height = 1.70f;
+    }
+
+    public void sit() {
+        System.out.println("I can sit.");
+    }
+
+    public void run() {
+        System.out.println("I can run.");
+    }
+
+    public void walk() {
+        System.out.println("I can walk.");
+    }
+
+    public void talk() {
+        System.out.println("I can talk.");
+    }
+    public void learnJava() {
+        System.out.println("I can learn Java language.");
+    }
+}
